03a0a081d0e6c51f81b0c94bcf0ff0eeae0878e4
Robbie Pitts <me@robbie-pitts.com>
2017-01-17 17:23:49 -0500

    Use fixturify in tests

--- test/find-lib-test.js
+++ test/find-lib-test.js
@@ -2,9 +2,11 @@
 
 const expect = require('chai').expect;
 const findLib = require('../lib/find-lib');
+const fixturify = require('fixturify');
 const path = require('path');
-const fs = require('fs');
 const rimrafSync = require('rimraf').sync;
+
+const NODE_MODULES_PATH = path.join(__dirname, '../node_modules');
 
 describe('find-lib', function() {
   describe('when module is not defined in package.json', function() {
@@ -19,13 +21,13 @@ describe('find-lib', function() {
     it('finds directory for given dependency', function() {
       let foundLib = findLib('foo');
 
-      expect(foundLib).to.eq(path.join(__dirname, '..', 'node_modules', 'foo', 'lib'));
+      expect(foundLib).to.eq(path.join(NODE_MODULES_PATH, 'foo', 'lib'));
     });
 
     it('finds directory for given dependency with optional libPath', function() {
       let foundLib = findLib('foo', 'bar');
 
-      expect(foundLib).to.eq(path.join(__dirname, '..', 'node_modules', 'foo', 'bar'));
+      expect(foundLib).to.eq(path.join(NODE_MODULES_PATH, 'foo', 'bar'));
     });
   });
 
@@ -41,17 +43,18 @@ describe('find-lib', function() {
     it('finds module directory for given dependency when module is defined in package.json', function() {
       let foundLib = findLib('foo');
 
-      expect(foundLib).to.eq(path.join(__dirname, '..', 'node_modules', 'foo', 'dist'));
+      expect(foundLib).to.eq(path.join(NODE_MODULES_PATH, 'foo', 'dist'));
     });
   });
 });
 
 function setupFakePackage(packageJson) {
-  fs.mkdirSync(path.join(__dirname, '../node_modules/foo'));
-  fs.writeFileSync(path.join(__dirname, '../node_modules/foo/package.json'), JSON.stringify(packageJson));
+  fixturify.writeSync(path.join(NODE_MODULES_PATH, 'foo'), {
+    'package.json': JSON.stringify(packageJson)
+  });
 }
 
 function teardownFakePackage() {
   delete require.cache[require.resolve('foo/package')];
-  rimrafSync(path.join(__dirname, '../node_modules/foo'));
+  rimrafSync(path.join(NODE_MODULES_PATH, 'foo'));
 }
